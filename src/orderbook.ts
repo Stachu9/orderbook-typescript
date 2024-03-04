5de113c9f0f724e079fdffff41ba94ba7766bcc4
stanislaw <starodfirma@gmail.com>
2024-03-04 17:25:06 +0100

    Class Orderbook implemented (type Map)

--- src/orderbook.ts
+++ src/orderbook.ts
@@ -1,37 +1,36 @@
 class Orderbook {
 
-  private orders
-
-  constructor() {}
+  private orders: Map<number, Order> = new Map()
 
   public addAction(action: Action): void {
-
-    if (action.orderId in this.orders) {
-      this.orders[action.orderId].addAction(action);
+    if (this.orders.has(action.orderId)) {
+      this.orders.get(action.orderId).addAction(action);
     } else {
-      this.orders[action.orderId] = new Order(action)
+      this.orders.set(action.orderId, new Order(action));
     }
   }
 
   public findBestOfferToBuy(): {price: number; quantity: number} {
+    let bestOfferToBuy: {price: number; quantity: number};
 
-    let bestOfferToBuy = {};
-
-    for (let order of this.orders) {
+    this.orders.forEach((order: Order): void => {
       if (order.getOrderType() === OrderType.SELL) {
         if (bestOfferToBuy) {
           if (bestOfferToBuy.price === order.getPrice()) {
             bestOfferToBuy.quantity += order.getQuantity();
           } else if (bestOfferToBuy.price > order.getPrice()) {
             bestOfferToBuy.price = order.getPrice();
-            bestOfferToBuy.quantity = order.getQuantity;
+            bestOfferToBuy.quantity = order.getQuantity();
           }
         } else {
-          bestOfferToBuy.price = order.getPrice();
-          bestOfferToBuy.quantity = order.getQuantity();
+          bestOfferToBuy = {
+            price: order.getPrice(),
+            quantity: order.getQuantity()
+          };
         }
       }
-    }
+    });
+
     return bestOfferToBuy;
   }
 
